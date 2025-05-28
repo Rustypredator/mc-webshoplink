4df9605f47cc606a57a75ac2b6f4b1cdbe87e88f
Rustypredator <contact@rusty.info>
2025-05-28 18:32:10 +0200

    remove traces from demo mod

--- src/main/java/info/rusty/webshoplink/Webshoplink.java
+++ src/main/java/info/rusty/webshoplink/Webshoplink.java
@@ -47,9 +47,6 @@ public class Webshoplink {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
